cce68e1d750db7bbd37c0a2a935fc72124098500
Maik Messerschmidt <maik.messerschmidt@gmx.net>
2019-07-06 08:31:40 +0200

    Added pulses and removed bolts.

--- thunderstorm/main.lua
+++ thunderstorm/main.lua
@@ -20,7 +20,10 @@ function love.load()
     -- graphics
     SCREEN_WIDTH, SCREEN_HEIGHT = love.graphics.getDimensions()
 
+    sky = love.graphics.newImage("res/sky.png")
     background = love.graphics.newImage("res/background.png")
+    foreground = love.graphics.newImage("res/foreground.png")
+
     lightning = {
          love.graphics.newImage("res/lightning3.png")
     }
@@ -36,15 +39,31 @@ function love.load()
 end
 
 function love.draw()
+    love.graphics.draw(sky, 0, 0)
     love.graphics.draw(background, 0, 0)
 
     -- draw bolts
     for _, bolt in pairs(bolts) do
+        -- no bolts are drawn right now
+        --[[
         if bolt.duration > 0 then
             love.graphics.draw(
                 bolt.image, bolt.x, bolt.y, 0, bolt.scale, bolt.scale)
         end
+        ]]--
+
+        -- draw pulses
+        local pulse = bolt.pulses[bolt.pulse_idx]
+        if pulse ~= nil and pulse.eta <= 0 and pulse.duration > 0 then
+            print(string.format("Drawing pulse: %d", bolt.pulse_idx))
+            local r, g, b, a = love.graphics.getColor()
+            love.graphics.setColor(1, 1, 1, pulse.alpha)
+            love.graphics.rectangle("fill", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+            love.graphics.setColor(r, g, b, a)
+        end
     end
+
+    love.graphics.draw(foreground, 0, 0)
 
     rain_gfx:draw()
 
@@ -64,6 +83,22 @@ function love.update(dt)
         bolt.duration = bolt.duration - dt
         bolt.thunder_eta = bolt.thunder_eta - dt
 
+        -- proceed to the next pulse, if the
+        -- current has already been drawn
+        local pulse = bolt.pulses[bolt.pulse_idx]
+
+        if pulse ~= nil then
+            if pulse.eta < 0 then
+                if pulse.duration < 0 then
+                    bolt.pulse_idx = bolt.pulse_idx + 1
+                else
+                    pulse.duration = pulse.duration - dt
+                end
+            else
+                pulse.eta = pulse.eta - dt
+            end
+        end
+
         if bolt.thunder_eta <= 0 then
             -- play sound and "discard" bolt
             love.audio.play(bolt.sound)
@@ -80,7 +115,7 @@ function love.update(dt)
     -- Add new bolts, if required.
     next_bolt_in = next_bolt_in - dt
     if next_bolt_in < 0 then
-        next_bolt_in = 0.1 + love.math.random()
+        next_bolt_in = 5 + love.math.random(20)
         last_bolt = generate_bolt()
         table.insert(bolts, last_bolt)
     end
@@ -119,9 +154,39 @@ function generate_bolt()
 
     bolt.scale = 0.2 + love.math.random() / 4
 
+    -- A bolt has an initial pulse and up to 3 more
+    local pulse_count = love.math.random(2, 3)
+    bolt.pulses = {
+        {eta=0, duration=randfloat(0.1, 0.2), alpha=0.5},
+        {eta=randfloat(0.5, 1.0), duration=randfloat(0.1, 0.2), alpha=0.3}
+    }
+    for i = 1, pulse_count - 1 do
+        local eta = randfloat(0.1, 0.2)
+        local duration = randfloat(0.1, 0.2)
+        table.insert(bolt.pulses, {eta=eta, duration=duration, alpha=0.3})
+    end
+    bolt.pulse_idx = 1
+
     bolt.duration = 0.3 -- love.math.random(0.2, 0.2)
     bolt.thunder_eta = bolt.distance / 300
     bolt.sound = thundersounds[love.math.random(1, #thundersounds)]
 
     return bolt
+end
+
+function randfloat(...)
+    arg1, arg2 = ...
+    local min, max
+    if arg1 == nil and arg2 == nil then
+        min = 0
+        max = 1
+    elseif arg2 == nil then
+        min = 0
+        max = arg1
+    else
+        min = arg1
+        max = arg2
+    end
+
+    return min + love.math.random() * (max - min)
 end
